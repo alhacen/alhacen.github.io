6ce9c5661b2c589d99be82991bac5bae1ad91c71
alhaqhassan <alhaqhassan@gmail.com>
2022-09-29 22:10:31 +0530

    added masked e-mail

---
+++ src/components/footer.js
@@ -0,0 +1,72 @@
+import React from 'react'
+const Footer = () =>{
+    return(
+        <div>
+            <div className="bg-white flex justify-between">
+                <div className="ml-4 mb-4 relative">
+                    <a href="https://github.com/alhaqhassan/alhaqhassan.github.io" className="absolute bottom-0">
+                        <div className=" flex items-center gap-4  p-2 border-2 border-black">
+                            <svg width="37" viewBox="0 0 167 135" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M167 107.5C167 122.688 154.688 135 139.5 135C124.312 135 112 122.688 112 107.5C112 92.3122 124.312 80 139.5 80C154.688 80 167 92.3122 167 107.5ZM125.564 107.5C125.564 115.197 131.803 121.436 139.5 121.436C147.197 121.436 153.436 115.197 153.436 107.5C153.436 99.8035 147.197 93.5642 139.5 93.5642C131.803 93.5642 125.564 99.8035 125.564 107.5Z" fill="black" />
+                                <path d="M55 107.5C55 122.688 42.6878 135 27.5 135C12.3122 135 0 122.688 0 107.5C0 92.3122 12.3122 80 27.5 80C42.6878 80 55 92.3122 55 107.5ZM13.445 107.5C13.445 115.262 19.7376 121.555 27.5 121.555C35.2624 121.555 41.555 115.262 41.555 107.5C41.555 99.7376 35.2624 93.445 27.5 93.445C19.7376 93.445 13.445 99.7376 13.445 107.5Z" fill="black" />
+                                <path d="M165 33.5C165 48.6878 152.688 61 137.5 61C122.312 61 110 48.6878 110 33.5C110 18.3122 122.312 6 137.5 6C152.688 6 165 18.3122 165 33.5ZM123.445 33.5C123.445 41.2624 129.738 47.555 137.5 47.555C145.262 47.555 151.555 41.2624 151.555 33.5C151.555 25.7376 145.262 19.445 137.5 19.445C129.738 19.445 123.445 25.7376 123.445 33.5Z" fill="black" />
+                                <path d="M46 101H116V114H46V101Z" fill="black" />
+                                <path d="M55 108C81.7456 107.471 83.7416 100.897 86.5 89L91 65.5C94 53 95.3502 38.3506 102.5 36.5C111.437 34.1867 116.5 35.9743 116.5 34.3219" stroke="black" strokeWidth="12" />
+                            </svg>
+                            <div className="font-bold text-xl">Fork</div>
+                        </div>
+                    </a>
+                </div>
+                <div className="p-10 text-right flex flex-col gap-2">
+                    <div className="text-2xl font-bold">Contact</div>
+                    
+                    <div className="flex gap-4 flex-row-reverse">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                            <path d="M0 0h24v24H0z" fill="none" />
+                            <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+                            </svg>
+                        </div>
+                        <div>gwz73gj19@mozmail.com</div>
+                    </div>
+                    <a href="https://github.com/alhaqhassan">
+                        <div className="flex gap-4 flex-row-reverse">
+                            <div>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={24}
+                                height={24}
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M15.5 22.7h-.1l-.1-.1V22v-2.5c0-.7-.1-1.3-.4-1.8 2.3-.4 4.8-1.6 4.8-6.1 0-1.2-.4-2.3-1.1-3.2.2-.6.3-1.7-.2-3.1l-.3-.3s-.2-.1-.4-.1c-.6 0-1.5.2-3 1.2-.8-.1-1.7-.3-2.7-.3-1 0-1.9.1-2.8.3C7.8 5.2 6.8 5 6.2 5c-.2 0-.3.1-.4.1-.1 0-.3.2-.3.3-.5 1.4-.4 2.5-.2 3.1-.7.9-1.1 2-1.1 3.2 0 4.4 2.6 5.6 4.8 6.1-.1.2-.2.5-.3.8-.2.1-.5.2-.9.2s-.8-.1-1.1-.4l-.1-.1c-.1-.1-.1-.2-.2-.2l-.1-.1-.1-.1c0-.1-.8-1.3-2.2-1.4-.5 0-.9.2-1 .5-.2.5.4.9.7 1.1 0 0 .6.3 1 1.4.2.7 1.1 2 3.2 2h.7v1.4l-.1.1s-.1 0 0 0C4 21.2 1 17 1 12.3c0-6.1 4.9-11 11-11s11 4.9 11 11c0 4.7-3 8.9-7.5 10.4z" />
+                                </svg>
+                            </div>
+                            <div>alhaqhassan</div>
+                        </div>
+                    </a>
+                    <a href="https://www.linkedin.com/in/alhaqhassan">
+                        <div className="flex gap-4 flex-row-reverse">
+                            <div>
+                            <svg
+                                height={20}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 310 310"
+                            >
+                                <path d="M72.16 99.73H9.927a5 5 0 00-5 5v199.928a5 5 0 005 5H72.16a5 5 0 005-5V104.73a5 5 0 00-5-5zM41.066.341C18.422.341 0 18.743 0 41.362 0 63.991 18.422 82.4 41.066 82.4c22.626 0 41.033-18.41 41.033-41.038C82.1 18.743 63.692.341 41.066.341zM230.454 94.761c-24.995 0-43.472 10.745-54.679 22.954V104.73a5 5 0 00-5-5h-59.599a5 5 0 00-5 5v199.928a5 5 0 005 5h62.097a5 5 0 005-5V205.74c0-33.333 9.054-46.319 32.29-46.319 25.306 0 27.317 20.818 27.317 48.034v97.204a5 5 0 005 5H305a5 5 0 005-5V194.995c0-49.565-9.451-100.234-79.546-100.234z" />
+                            </svg>
+                            </div>
+                            <div>alhaqhassan</div>
+                        </div>
+                    </a>
+                </div>
+                </div>
+                <div className="flex justify-center pb-3" style={{wordSpacing:'3px'}}>
+                    Made with 
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="red"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
+                    by <span className="font-bold">Ahmad; </span>
+                </div>
+
+        </div>
+    )
+}
+export default Footer;
